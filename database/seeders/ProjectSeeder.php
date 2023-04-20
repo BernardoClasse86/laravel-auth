98862a0bbfa63289522ca4c8f79c6fec8fe765e5
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-04-20 15:02:44 +0200

    FEAT table seeded

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,20 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+
+            $project = new Project();
+
+            $project->title = $faker->unique()->sentence($faker->numberBetween(1, 4));
+            $project->client_name = $faker->optional()->name();
+            $project->description = $faker->optional()->paragraph($faker->numberBetween(4, 6), true);
+            $project->project_url = $faker->url();
+            $project->project_date = $faker->dateTimeBetween('-1 year', 'now');
+            $project->slug = Str::slug($project->title, '-');
+
+            $project->save();
+        }
     }
 }
