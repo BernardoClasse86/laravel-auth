295fa86e5842d72958ebb421b875e5b330882cc9
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-04-20 16:59:05 +0200

    FEAT show added

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    ciao
+    <div class="my-container">
+
+        <div class="my-card">
+
+            <div class="my-row heading">
+                <img class="project-thumb" src="/images/default-project.png" alt="computer-thumb">
+                <div><strong>Client:</strong> {{$project->client_name}}</div>
+            </div>
+
+            <div class="my-row details">
+                <div>
+                    Project Date: {{$project->project_date}}
+                </div>
+
+                <div>
+                    <a class="btn btn-sm btn-primary" href="{{$project->project_url}}">Project Link</a>
+                </div>
+            </div>
+
+            <div class="my-row">
+                <div class="my-title">
+                    {{$project->title}}
+                </div>
+            </div>
+
+            <div class="my-row">
+                <div class="my-desc">
+                    {{$project->description}}
+                </div>
+            </div>
+
+            <div class="my-row edit">
+                <a href="{{route('projects.edit', $project)}}" class="btn btn-sm btn-warning">Edit Project</a>
+            </div>
+            
+        </div>
+
+    </div>
 @endsection
